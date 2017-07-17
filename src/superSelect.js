0f76cd8f4d08d6c45a24817f0c65d0c0d54830fc
Hao Jin <haojinhj@gmail.com>
2017-07-17 22:20:36 +0800

    Update superSelect.js

--- src/superSelect.js
+++ src/superSelect.js
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2014 Small Batch, Inc.
+ * Copyright 2014
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
